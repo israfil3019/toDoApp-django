ea3d117521902bf8b77cf0d5b40a9384d7d92d3e
israfil <israfil3019@gmail.com>
2021-07-15 13:24:54 +0300

    add add page

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, todo_list
+from .views import home, todo_add, todo_list
 
 urlpatterns = [
     path("", home, name="home"),
     path("list/", todo_list, name="list"),
+    path("add/", todo_add, name="add"),
 
 ]
--- src/todo/views.py
+++ src/todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoForm
 
 
 def home(request):
@@ -12,3 +13,16 @@ def todo_list(request):
         "todos": todos
     }
     return render(request, "todo/todo_list.html", context)
+
+def todo_add(request):
+    form = TodoForm()
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+    context = {
+        'form':form,
+    }
+    return render(request, "todo/todo_add.html", context)
+
